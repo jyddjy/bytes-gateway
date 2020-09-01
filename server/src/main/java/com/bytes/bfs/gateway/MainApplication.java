6b10fe67916f44a320e9ecd5c28fe14b41e6f025
麻亮 <maliang@newhope.cn>
2020-09-01 13:59:11 +0800

    feat(*): 修改网关配置

--- server/src/main/java/com/bytes/bfs/gateway/MainApplication.java
+++ server/src/main/java/com/bytes/bfs/gateway/MainApplication.java
@@ -1,5 +1,7 @@
 package com.bytes.bfs.gateway;
 
+import com.bytes.box.commons.base.annocation.EnableBox;
+import com.bytes.box.commons.web.flux.resolve.WebFluxExceptionResolve;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
@@ -8,6 +10,7 @@ import org.springframework.context.annotation.ComponentScan;
 @Slf4j
 @ComponentScan(value = {"com.bytes.bfs"})
 @SpringCloudApplication
+@EnableBox(imports = {WebFluxExceptionResolve.class})
 public class MainApplication {
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
